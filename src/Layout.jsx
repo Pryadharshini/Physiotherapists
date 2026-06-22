f720d923f6715fb277f26af0d97ebbfc7ee01b90
shrivaishnavibhoopalakrishnan <shrivaishnavi19102002@gmail.com>
2026-06-22 23:39:49 +0530

    Changes made

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Components/navbar";
 import Footer from "./Components/footer";
 import PopupForm from "./Components/PopupForm";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
